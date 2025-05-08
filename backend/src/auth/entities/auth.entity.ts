2fbcbc4e22e91a70764f471ee71fb831bd8d0de3
Mohammed <elghanammohammed465@gmail.com>
2025-05-08 04:34:51 +0200

    fix & feat: fixing error of typing the env variable and add entites of user

--- backend/src/auth/entities/auth.entity.ts
+++ backend/src/auth/entities/auth.entity.ts
@@ -1,1 +1,16 @@
-export class Auth {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({ timestamps: true })
+export class Auth {
+
+    @Prop()
+    name: string;
+
+    @Prop({ unique:[true, 'Duplicate email enterd']})
+    email: string;
+
+    @Prop()
+    password: string;
+}
+
+export const AuthSchema = SchemaFactory.createForClass(Auth);
