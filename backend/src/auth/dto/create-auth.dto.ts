ddb21212c00da908af4ebec7bab505b9c49e53a4
Mohammed <elghanammohammed465@gmail.com>
2025-05-08 03:29:05 +0200

    feat: added code of login dto and install packages class-validator and class-transformer

--- backend/src/auth/dto/create-auth.dto.ts
+++ backend/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+export class CreateAuthDto {
+    
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    readonly name?: string;
+
+    
+    @IsString()
+    @IsEmail()
+    readonly email: string;
+
+    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    readonly password: string;
+}
