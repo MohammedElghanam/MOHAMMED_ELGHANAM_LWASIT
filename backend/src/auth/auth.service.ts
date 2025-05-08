cc4be8ec2bc8500afe88a12a683b6683f57b4c33
Mohammed <elghanammohammed465@gmail.com>
2025-05-08 04:57:47 +0200

    feat: added function of register user and added configuration of jwt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Auth } from './entities/auth.entity';
+import * as bcrypt from 'bcryptjs';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  constructor( 
+      @InjectModel(Auth.name) private authModel: Model<Auth>, 
+      private jwtService: JwtService        
+  ){}
+
+  async register ( createAuthDto: CreateAuthDto ): Promise<{ message: string, user: object | null }> {
+      const { name, email, password }= createAuthDto;
+
+      const existingUser = await this.authModel.findOne({ email });
+      if (existingUser) throw new BadRequestException('Email is already in use');
+
+      try {
+          const hashPassword = await bcrypt.hash(password, 10)
+          const user= await this.authModel.create({
+              name,
+              email,
+              password: hashPassword
+          })
+
+          return {
+              message: "The user has been created successfully and is ready to use the system.",
+              user
+          }
+      } catch (error) {
+          console.error('Error creating user:', error);
+          throw new BadRequestException('Failed to create user in the database');
+      }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
